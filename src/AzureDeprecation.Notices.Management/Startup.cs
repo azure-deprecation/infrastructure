55ba578e6c41177c3109e6caef74df641fd33740
Tom Kerkhove <kerkhove.tom@gmail.com>
2021-02-26 08:41:17 +0100

    Support more clouds & Azure Application Insights (#32)
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/AzureDeprecation.Notices.Management/Startup.cs
+++ src/AzureDeprecation.Notices.Management/Startup.cs
@@ -1,11 +1,9 @@
 ﻿using AutoMapper;
-using AzureDeprecation.Contracts.Messages.v1;
 using AzureDeprecation.Integrations.GitHub.Repositories;
 using AzureDeprecation.Notices.Management.MessageHandlers;
 using AzureDeprecation.Runtimes.AzureFunctions;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
-using Octokit;
 
 [assembly: FunctionsStartup(typeof(AzureDeprecation.Notices.Management.Startup))]
 namespace AzureDeprecation.Notices.Management
