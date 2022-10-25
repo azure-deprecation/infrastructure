04c96050546bbc63c4a7dca9ea562127a608156e
Tom Kerkhove <kerkhove.tom@gmail.com>
2022-10-25 15:32:00 +0200

    Update AzureService.cs

--- src/AzureDeprecation.Contracts/Enum/AzureService.cs
+++ src/AzureDeprecation.Contracts/Enum/AzureService.cs
@@ -115,6 +115,7 @@ namespace AzureDeprecation.Contracts.Enum
         SqlEdge,
         SqlManagedInstance,
         Stack,
+        StaticWebApp,
         Storage,
         StoreSimple,
         StreamAnalytics,
